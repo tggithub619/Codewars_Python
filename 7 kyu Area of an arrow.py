73509cc96e979d139e15f6bf048bae7804f7e45f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-03 13:15:28 -0800

    2

---
+++ 7 kyu Area of an arrow.py
@@ -0,0 +1,7 @@
+#https://www.codewars.com/kata/589478160c0f8a40870000bc/solutions/python/me/best_practice
+
+def arrow_area(a, b):
+    return (0.5 * a * 0.5*b)
+
+def arrow_area(a, b):
+    return a * b / 4.0
