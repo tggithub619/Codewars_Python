d7e4f1343ebde3f82688df47e7d055167db846d6
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-03 12:54:13 -0800

    2

---
+++ 8 kyu Century From Year.py
@@ -0,0 +1,8 @@
+#https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/solutions/python
+
+def century(year):
+    return (year + 99) // 100
+
+def century(year):
+    # Finish this :)
+    return int((year-1)/100) + 1
