819e00e1695c866806a20e496348a452ffafbe26
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-08 20:41:46 -0800

    1

---
+++ 7 kyu Greet Me.py
@@ -0,0 +1,7 @@
+#https://www.codewars.com/kata/535474308bb336c9980006f2/solutions/python
+
+def greet(name):
+    return f'Hello {name.title()}!'
+
+def greet(name):
+    return f"Hello {name.capitalize()}!"
