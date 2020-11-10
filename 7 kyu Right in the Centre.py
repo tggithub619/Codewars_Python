a30dbff519fe15e0c02db4644d06f3b498376a27
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-09 22:48:51 -0800

    1

---
+++ 7 kyu Right in the Centre.py
@@ -0,0 +1,16 @@
+#https://www.codewars.com/kata/5f5da7a415fbdc0001ae3c69/train/python
+
+def is_in_middle(seq):
+    while len(seq) >= 3:
+        if seq == 'abc' or seq[1:] == 'abc' or seq[:-1] == 'abc':
+            return True
+        seq = seq[1:-1]
+    return False
+
+def is_in_middle(s):
+    while len(s) > 4:
+        s = s[1:-1]
+
+    if 'abc' in s:
+        return True
+    return False
