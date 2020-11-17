eb2eadcc7527f1e9bdfe2c2b610f5259248dbddb
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-11-16 22:03:25 -0800

    1

--- 6 kyu Pair of gloves.py
+++ 6 kyu Pair of gloves.py
@@ -3,3 +3,14 @@
 def number_of_pairs(gloves):
     dct = {color: gloves.count(color) for color in gloves}
     return sum(count //2 for count in dct.values())
+
+def number_of_pairs(gloves):
+    lst = []
+    n = 0
+    for i in gloves:
+        if i in lst:
+            lst.remove(i)
+            n = n+1
+        else:
+            lst.append(i)
+    return n
