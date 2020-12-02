a3c3620e4936d01c001f01650b67cccbd6cc7d2b
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-12-01 21:44:33 -0800

    1

---
+++ 8 kyu Count the Monkeys.py
@@ -0,0 +1,7 @@
+#https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/solutions/python
+
+def monkey_count(n):
+    return range(1, n+1)
+
+def monkey_count(n):
+    return [i+1 for i in range(n)]
