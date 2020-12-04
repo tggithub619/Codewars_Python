982a34bc6976095aa7c1470ef7d08fcd94fa637f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-12-03 16:16:00 -0800

    1

---
+++ 7 kyu Moving Average.py
@@ -0,0 +1,15 @@
+#https://www.codewars.com/kata/5c745b30f6216a301dc4dda5/solutions/python/me/best_practice
+
+def moving_average(v,n):
+    if n == 0 or n > len(v):
+        return None
+    return [sum(v[i:i+n])/n for i in range(len(v)-n+1)]
+
+
+def moving_average(v, n):
+    if n == 0 or n > len(v):
+        return None
+    a = []
+    for i in range(len(v) - n + 1):
+        a.append(sum(v[i:i + n]) / n)
+    return a
