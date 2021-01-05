66f2bf7ab95d336db7afb8872c3d0de0167339e2
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-01-05 13:38:09 -0800

    1

--- 7 kyu Calculator Coin Combination.py
+++ 7 kyu Calculator Coin Combination.py
@@ -7,3 +7,12 @@ def coin_combo(c):
         arr.append(c//i)
         c = c%i
     return arr[::-1]
+
+
+def coin_combo(cents):
+    rest = 0
+    q = cents // 25
+    d = cents % 25 // 10
+    n = cents % 25 % 10 // 5
+    p = cents % 25 % 10 % 5
+    return [p, n, d, q]
