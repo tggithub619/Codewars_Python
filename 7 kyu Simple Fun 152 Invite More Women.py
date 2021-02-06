1bd8083fe3c2433dcb24d92ea5812e8db166c16f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-05 16:19:31 -0800

    1

---
+++ 7 kyu Simple Fun 152 Invite More Women.py
@@ -0,0 +1,11 @@
+#https://www.codewars.com/kata/58acfe4ae0201e1708000075
+
+def invite_more_women(arr):
+    return sum(arr) > 0
+
+#Test.assert_equals(invite_more_women([1, -1, 1]), True)
+  #  Test.assert_equals(invite_more_women([-1, -1, -1]), False)
+# Test.assert_equals(invite_more_women([1, -1]), False)
+ #   Test.assert_equals(invite_more_women([1, 1, 1]), True)
+  #  Test.assert_equals(invite_more_women([]), False)
+
