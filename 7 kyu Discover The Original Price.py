ac3fd72a30d41ad296a4685b289d038b45add6b6
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-08 12:51:26 -0800

    1

--- 7 kyu Discover The Original Price.py
+++ 7 kyu Discover The Original Price.py
@@ -2,3 +2,9 @@
 
 def discover_original_price(discounted_price, sale_percentage):
     return round(discounted_price / ((100 - sale_percentage) * 0.01), 2)
+
+# Test.assert_equals(discover_original_price(75,25),100)
+# Test.assert_equals(discover_original_price(25,75),100)
+# Test.assert_equals(discover_original_price(75.75,25),101)
+# Test.assert_equals(discover_original_price(373.85,11.2),421)
+# Test.assert_equals(discover_original_price(458.2,17.13),552.91)
