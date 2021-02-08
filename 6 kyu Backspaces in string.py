6a574d9923b8aaa597bf875b17aa92ab06f69aa4
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-08 11:13:00 -0800

    1

--- 6 kyu Backspaces in string.py
+++ 6 kyu Backspaces in string.py
@@ -14,3 +14,19 @@ def clean_string(s):
 # Test.assert_equals(clean_string('abc####d##c#'), "" )
 # Test.assert_equals(clean_string("#######"), "" )
 # Test.assert_equals(clean_string(""), "" )
+
+def clean_string(s):
+    arr = []
+    for a in s:
+        if a != '#':
+            arr.append(a)
+        elif arr:
+            arr.pop()
+    return ''.join(arr)
+
+def clean_string(string):
+    out = ''
+    for x in string:
+        if x != '#': out += x
+        else: out = out[:-1]
+    return out
