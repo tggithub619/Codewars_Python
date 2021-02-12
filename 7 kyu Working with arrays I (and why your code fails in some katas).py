8906cacc3501df8247424ca18d12a56206e87700
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-12 11:15:58 -0800

    1

--- 7 kyu Working with arrays I (and why your code fails in some katas).py
+++ 7 kyu Working with arrays I (and why your code fails in some katas).py
@@ -1,2 +1,8 @@
+#https://www.codewars.com/kata/5a4ff3c5fd56cbaf9800003e
 
+def without_last(lst):
+    return lst[:-1]
+
+# test.assert_equals(without_last([1, 2, 3, 4, 5]), [1, 2, 3, 4])
+#         test.assert_equals(without_last([9, 7, 6, 9]), [9, 7, 6])
 
