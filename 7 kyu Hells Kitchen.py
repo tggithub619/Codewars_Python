07df40c4189aac01525d278e263c0785940aea15
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-16 22:58:17 -0800

    1

---
+++ 7 kyu Hells Kitchen.py
@@ -0,0 +1,15 @@
+#https://www.codewars.com/kata/57d1f36705c186d018000813
+
+def gordon(a):
+    print(a)
+    a= a.upper().replace("A", "@")
+    res = ''.join(["*" if el in 'EIOU' else el for el in a ])
+    res = " ".join([el+ '!!!!' for el in res.split()])
+    print(res)
+    return res
+
+#  test.assert_equals(gordon('What feck damn cake'), 'WH@T!!!! F*CK!!!! D@MN!!!! C@K*!!!!')
+#         test.assert_equals(gordon('are you stu pid'), '@R*!!!! Y**!!!! ST*!!!! P*D!!!!')
+#         test.assert_equals(gordon('i am a chef'), '*!!!! @M!!!! @!!!! CH*F!!!!')
+#         test.assert_equals(gordon('dont you talk tome'), 'D*NT!!!! Y**!!!! T@LK!!!! T*M*!!!!')
+#         test.assert_equals(gordon('how dare you feck'), 'H*W!!!! D@R*!!!! Y**!!!! F*CK!!!!')
