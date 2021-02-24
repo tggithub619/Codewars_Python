58ec4d4b7f9325228f591190b62cc267a7f3d277
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-24 11:52:44 -0800

    1

--- 7 kyu Is this a triangle.py
+++ 7 kyu Is this a triangle.py
@@ -2,3 +2,12 @@
 
 def is_triangle(a, b, c):
     return a + b > c and b + c > a and c + a > b
+
+#  test.assert_equals(is_triangle(1, 2, 2), True, "didn't work when sides were 1, 2, 2")
+#         test.assert_equals(is_triangle(7, 2, 2), False, "didn't work when sides were 7, 2, 2")
+#         test.assert_equals(is_triangle(1, 2, 3), False, "didn't work when sides were 1, 2, 3")
+#         test.assert_equals(is_triangle(1, 3, 2), False, "didn't work when sides were 1, 3, 2")
+#         test.assert_equals(is_triangle(3, 1, 2), False, "didn't work when sides were 3, 1, 2")
+#         test.assert_equals(is_triangle(5, 1, 2), False, "didn't work when sides were 5, 1, 2")
+#         test.assert_equals(is_triangle(1, 2, 5), False, "didn't work when sides were 1, 2, 5")
+#         test.assert_equals(is_triangle(2, 5, 1), False, "didn't work when sides were 2, 5, 1")
--- 8 kyu I love you, a little , a lot, passionately ... not at all.py
+++ 8 kyu I love you, a little , a lot, passionately ... not at all.py
@@ -14,3 +14,7 @@ def how_much_i_love_you(nb_petals):
 
 def how_much_i_love_you(nb_petals):
     return ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][nb_petals % 6 - 1]
+
+# test.assert_equals(how_much_i_love_you(7),"I love you")
+#         test.assert_equals(how_much_i_love_you(3),"a lot")
+#         test.assert_equals(how_much_i_love_you(6),"not at all")
