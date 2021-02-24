15c00656c12aa4637403be93f4384ba246dbfee5
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-24 11:52:32 -0800

    1

--- 7 kyu Arithmetic sequence - sum of n elements.py
+++ 7 kyu Arithmetic sequence - sum of n elements.py
@@ -6,5 +6,12 @@ def arithmetic_sequence_sum(a, r, n):
         res += r * i + a
     return res
 
+# test.assert_equals(arithmetic_sequence_sum(3, 2, 20), 440)
+# test.assert_equals(arithmetic_sequence_sum(2, 2, 10), 110)
+# test.assert_equals(arithmetic_sequence_sum(1, -2, 10), -80)
+
 def arithmetic_sequence_sum(a, r, n):
-    return n * (a + a + ( n - 1) * r) / 2  
+    return n * (a + a + ( n - 1) * r) / 2
+
+def arithmetic_sequence_sum(a, r, n):
+    return sum(a + r*x for x in range(n))
--- 7 kyu Calculate Two People's Individual Ages.py
+++ 7 kyu Calculate Two People's Individual Ages.py
@@ -4,3 +4,6 @@ def get_ages(sum, d):
     a = (sum + d) / 2
     b = sum - a
     return None if sum < 0 or d < 0 or a < 0 or b < 0 else (max(a,b), min(a,b))
+
+# Test.assert_equals(get_ages(24, 4), (14, 10))
+# Test.assert_equals(get_ages(63, -14), None)
